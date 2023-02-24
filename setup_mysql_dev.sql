46d64f93ab50be58f931cec4663ea69dea47ed89
Ann Lau <101356391+Annedumo@users.noreply.github.com>
2023-02-24 18:51:05 +0300

    prepares a MySQL server for the project

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,6 @@
---preparing MYSQL server for the project
-
-CREATE DATABASE [IF NOT EXISTS] hbnb_test_db;
-CREATE USER [IF NOT EXISTS]'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PLIVILEDGES ON 'hbnb_test_db'.* TO 'hbnb_test_db'@'localhost';
-GRANT SELECT ON 'performance_schema'.* TO 'hbnb_test_db'@'localhost';
+-- prepares MYSQL server for the project
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEDGES ON 'hbnb_dev_db'.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON 'performance_schema'.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEDGES;
