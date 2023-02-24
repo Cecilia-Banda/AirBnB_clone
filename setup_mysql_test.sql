307f32691e19f4c69b9482139386b59b42492328
Ann Lau <101356391+Annedumo@users.noreply.github.com>
2023-02-24 18:11:28 +0300

    prepares a MySQL server for the project

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,4 @@
-/*prepares MYSQL server for the project*/
+--prepares MYSQL server for the project
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PLIVILEDGES ON 'hbnb_test_db'.* TO 'hbnb_test'@'localhost';
