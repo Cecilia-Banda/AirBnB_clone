d8ebc13f45d068285b5704ee318de7a61cafe25b
Ann-lau <a.lau@alustudenet.com>
2023-03-08 15:30:32 +0300

    fix

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,6 @@
 -- prepares MYSQL server for the project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-USE hbnb_dev_db;
-GRANT ALL PRIVILEDGES ON 'hbnb_dev_db'.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-FLUSH PRIVILEDGES;
+GRANT ALL PRIVILEGES ON 'hbnb_dev_db'.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT SELECT ON 'performance_schema'.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-FLUSH PRIVILEDGES;
+FLUSH PRIVILEGES;
