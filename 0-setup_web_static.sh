a6c0348355927be81bfd2787e4f24f41bc7b51a6
Ann Lau <101356391+Annedumo@users.noreply.github.com>
2023-03-13 17:26:37 +0300

    models

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,5 +8,6 @@ sudo mkdir -p /data/web_static/shared/
 echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -fs /data/web_static/releases/test/ /data/web_static/surrent
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i \"35i $STATIC" $SRC
-sudo service nginx restart
+sudo sed '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
+service nginx restart
+exit 0
